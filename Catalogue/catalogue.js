e13c19bc90760ee74cfc480ce3c7526c0ca15941
KaoriL <149507137+KaoriL@users.noreply.github.com>
2024-10-03 09:36:19 +0200

    Update catalogue.js

--- Catalogue/catalogue.js
+++ Catalogue/catalogue.js
@@ -1,44 +1,4 @@
 function toggleBar() {
-    let form = document.getElementById("searchBar");
-    form.classList.toggle("active");
-  }
-
-
-  //Liste bd
-
-  //Filtre Rechercche
-
-  //Template image
-
-  // Slide liste bd 
-
-  const slide = [
-    "Blacksad-03-Ame rouge.jpg",
-    "Elfes-01-Le crystal des Elfes bleus.jpg",
-    "Trolls de Troy-05-Les mal‚fices de la thaumaturge.jpg",
-  ];
-  
-  let auteur = ["Juan Diaz Canales", "Kyko Duartes", "Christophe Arleston"];
-  
-  let titre = ["Blacksad 03 ", "Efles", "Troll de Troy"];
-
-  let numero = 0;
-
-function ChangeSlide(sens) {
-  numero = numero + sens;
-  if (numero > slide.length - 1) numero = 0;
-  if (numero < 0) numero = slide.length - 1;
-
-  document.getElementById("bd").src = "/.vscode/0_assets/BD/albumsMini/" + slide[numero];
-  document.getElementById("Auteur").innerText = auteur[numero];
-  document.getElementById("Titre").innerText = titre[numero];
-  
+  let form = document.getElementById("searchBar");
+  form.classList.toggle("active");
 }
-
-console.log(slide);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          f
-
-
-  //Bouton retour 
-
-  
-  
