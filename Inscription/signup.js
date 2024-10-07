658c7223cbc30f0eb677ef12a669ac9a3a1149e5
Masihullah780 <roshanzamir2017@gmail.com>
2024-10-07 22:05:07 +0200

    Update signup.js

--- Inscription/signup.js
+++ Inscription/signup.js
@@ -117,15 +117,6 @@ window.addEventListener('load', function() {
     });
   }
 
-  // Toggle password visibility
-  if (togglePassword) {
-    togglePassword.addEventListener('click', function() {
-      const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-      passwordInput.setAttribute('type', type);
-      this.querySelector('i').classList.toggle('fa-eye');
-      this.querySelector('i').classList.toggle('fa-eye-slash');
-    });
-  }
 
   
 
